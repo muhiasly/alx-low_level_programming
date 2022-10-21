c2af466da6c9442adc79622152d5989214a25f93
muhiasly <muhiasylvester@gmail.com>
2022-10-21 10:46:18 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,11 +3,11 @@
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
-void positive_or_negative(int i)
+int main(void)
 {
 	int i;
 
-	i = 98;
+	i = 0;
 	postitive_or_negative(i);
 
 	return (0);
