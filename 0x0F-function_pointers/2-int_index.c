78ffcc57804d144304aa794562e27ca3425bcfc5
muhiasly <muhiasylvester@gmail.com>
2022-11-17 03:46:33 -0800

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,9 +6,7 @@
  * @array: array to search integer
  * @size: size of the array
  * @cmp: pointer to the comparing function
- * return:index of the first element for which
- * the cmp function does not return 0, or -1 if no match is found
- * or size is negative
+ * @return: integer
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
